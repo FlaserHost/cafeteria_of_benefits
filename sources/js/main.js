230a4b4de09f8cb190ccff5da7f95d96bbc0d19b
Dmitry <flaserhost@gmail.com>
2023-07-21 09:57:08 +0300

    initial commit

---
+++ sources/js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const benefitsBtns = document.querySelectorAll('.benefits-btn');
+   const group = document.querySelectorAll('.group');
+   const checkboxes = document.querySelectorAll('.benefit-check');
+   const scalesBtn = document.getElementById('scales-btn');
+
+   benefitsBtns.forEach((btn, _, original) => {
+       btn.addEventListener('click', e => {
+           original.forEach(item => item.classList.remove('active'));
+           e.target.classList.add('active');
+       });
+   });
+
+   group.forEach(item => {
+       item.addEventListener('click', e => {
+           const thisBrother = e.currentTarget.nextElementSibling;
+
+           let add;
+           let remove;
+           let groupBodyHeight;
+
+           if (e.currentTarget.classList.contains('closed')) {
+               remove = 'closed';
+               add = 'opened';
+               groupBodyHeight = thisBrother.querySelector('.group-table').getBoundingClientRect().height;
+           } else {
+               remove = 'opened';
+               add = 'closed';
+               groupBodyHeight = 0;
+           }
+
+           e.currentTarget.classList.remove(remove);
+           e.currentTarget.classList.add(add);
+
+           thisBrother.style.height = `${groupBodyHeight}px`;
+       });
+   });
+
+    checkboxes.forEach(checkbox => {
+       checkbox.addEventListener('change', () => {
+           const checkedAmount = document.querySelectorAll('.benefit-check:checked').length;
+           scalesBtn.innerText = checkedAmount;
+       });
+   });
+
+    // feedbacks swiper
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
